30f82569ea9a2f92b17b043ec19940afdd950a91
hoangquan-061199 <hoangquan061199@gmail.com>
2022-09-06 23:17:35 +0700

    add library customize-cra babel-plugin-module-resolver prettier CSS/SASS Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import HeaderOnly from '~/components/Layout/HeaderOnly';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+
+const publicRouters = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/search', component: Search, layout: null },
+];
+
+const privateRouters = [];
+
+export { publicRouters, privateRouters };
